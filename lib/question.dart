3c2758f49e5cc14a93ff0b9e4ca0c55a53936487
Aswin Menon <axwinmenon@gmail.com>
2024-02-29 17:02:29 +0530

    bi

--- lib/question.dart
+++ lib/question.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp/questionlist.dart';
+
 class Que extends StatefulWidget {
   const Que({Key? key}) : super(key: key);
 
@@ -8,56 +9,88 @@ class Que extends StatefulWidget {
 }
 
 class _QueState extends State<Que> {
-  List question=[
-    Quiz(qus: 'car has 4 wheels', ans:true),
-    Quiz(qus: 'human blood colour is yellow', ans:false),
-    Quiz(qus: 'virat kohli is a football player ', ans:false),
-    Quiz(qus: 'actor arjun ashokan is the son of actor ashokan', ans:true),
-    Quiz(qus: 'mannumel boys is a real story', ans:true),
-    Quiz(qus: 'devagiri college is located at thrissur', ans:true),
-    Quiz(qus: 'bike has 4 wheels', ans:true),
-    Quiz(qus: 'gpay is devloped using flutter', ans:true),
-    Quiz(qus: 'mammooty is 50 years old', ans:false),
-    Quiz(qus: 'there are 13 districts in kerala', ans:false),
+  List question = [
+    Quiz(qus: 'car has 4 wheels', ans: true),
+    Quiz(qus: 'human blood colour is yellow', ans: false),
+    Quiz(qus: 'virat kohli is a football player ', ans: false),
+    Quiz(qus: 'actor arjun ashokan is the son of actor ashokan', ans: true),
+    Quiz(qus: 'mannumel boys is a real story', ans: true),
+    Quiz(qus: 'devagiri college is located at thrissur', ans: true),
+    Quiz(qus: 'bike has 4 wheels', ans: true),
+    Quiz(qus: 'gpay is devloped using flutter', ans: true),
+    Quiz(qus: 'mammooty is 50 years old', ans: false),
+    Quiz(qus: 'there are 13 districts in kerala', ans: false),
   ];
-  int question_no=0;
-  void nextque(){
-    if(question_no<question.length){
+  int question_no = 0;
+  String result='';
+  void nextque() {
+    if (question_no < question.length) {
       question_no++;
     }
   }
-  String getqus(){
+
+  void answer(bool answer) {
+    print(answer);
+    if(answer==question[question_no].ans){
+      result='correct';
+    }
+    else{
+      result='wrong';
+    }
+  }
+  String getqus() {
     return question[question_no].qus;
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 50,),
-            Text(question[question_no].qus,
-              style: TextStyle(fontSize: 28,color: Colors.amber),),
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              question[question_no].qus,
+              style: TextStyle(fontSize: 28, color: Colors.amber),
+            ),
+            SizedBox(
+              height: 50,
+            ),
             Container(
                 height: 50,
                 width: 100,
                 child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(primary: Colors.amber,),
-                    onPressed: (){
-                      setState((){
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.amber,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        answer(true);
                         nextque();
+
                       });
-                      }, child: Text('TRUE'))),
-            SizedBox(height: 50,),
+                    },
+                    child: Text('TRUE'))),
+            SizedBox(
+              height: 50,
+            ),
             Container(
                 height: 50,
-                width:100,
-                child: ElevatedButton(onPressed: (){setState((){
-                  nextque();
-                });}, child: Text('FALSE')))
+                width: 100,
+                child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        answer(false);
+                        nextque();
+                      });
+                    },
+                    child: Text('FALSE'))),
+        SizedBox(height:20),
+        Text(result,style: TextStyle(color:Colors.white),),
           ],
         ),
       ),
